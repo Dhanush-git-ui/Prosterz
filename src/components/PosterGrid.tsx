a84a6ad3ae79f81f86d7330a18416d21c2024140
Dhanush Gopavaram <dhanushgopavaram06@gmail.com>
2025-02-25 04:16:35 +0000

    Add popstar posters
    Adds new posters to the popstar category.

--- src/components/PosterGrid.tsx
+++ src/components/PosterGrid.tsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import { motion } from "framer-motion";
 
@@ -68,6 +69,42 @@ const posters: Poster[] = [
     image: "public/lovable-uploads/269c6c48-824c-4c7b-ac22-293e10ff0b7f.png",
     title: "BMW M4 GT4",
     category: "cars"
+  },
+  {
+    id: 11,
+    image: "public/lovable-uploads/b5ce56fc-ab10-4e5d-aaa7-112410851752.png",
+    title: "Cinnamon Girl",
+    category: "popstars"
+  },
+  {
+    id: 12,
+    image: "public/lovable-uploads/e544cf84-c984-408e-8db2-690ce557c461.png",
+    title: "FEIN",
+    category: "popstars"
+  },
+  {
+    id: 13,
+    image: "public/lovable-uploads/d3c85a47-1931-43c0-aaef-21b35e7d9402.png",
+    title: "Die With A Smile",
+    category: "popstars"
+  },
+  {
+    id: 14,
+    image: "public/lovable-uploads/358bb3f5-0364-48c0-8295-bfb47d273098.png",
+    title: "Starboy",
+    category: "popstars"
+  },
+  {
+    id: 15,
+    image: "public/lovable-uploads/318e6db9-d1b1-4ef7-be5d-ef48753bb9a3.png",
+    title: "Cry For Me",
+    category: "popstars"
+  },
+  {
+    id: 16,
+    image: "public/lovable-uploads/a5c5c4a8-ca40-4862-8f20-b74c93a67fcd.png",
+    title: "CO2",
+    category: "popstars"
   }
 ];
 
